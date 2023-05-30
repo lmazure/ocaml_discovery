3e519809d95f8463d119834a1b57a4ba5162ba47
Laurent Mazuré <laurent@gilgamesh>
2023-05-30 22:40:49 +0200

    Rewrote using records

--- ramsey2/lib/ramsey.ml
+++ ramsey2/lib/ramsey.ml
@@ -1,21 +1,44 @@
 type color = Green | Blue
 
-(* accessors *)
-let get_color_of_cell cell  = match cell with
-  | ( color, _, _, _) -> color
+let color_to_string color =
+  match color with
+  | Green -> "G"
+  | Blue -> "B";;
 
-let get_left_of_cell cell  = match cell with
-  | ( _, left, _, _) -> left
+(* a cell is defined by
+  - its colors  
+  - the list of cells at its left (starting from the nearest one)
+  - the list of cells on the left-above diagonal (starting from the nearest one)
+  - the list of cells above (starting from the nearest one)
+*)
+type cell = { color: color; left: cell list; diag: cell list; above: cell list }
 
-let get_diag_of_cell cell  = match cell with
-  | ( _, _, diag, _) -> diag
+let rec cell_to_debug_string cell =
+  "[color=" 
+  ^ (color_to_string cell.color )
+  ^ ",left="
+  ^ (String.concat "" (List.map (fun x -> cell_to_debug_string x) cell.left))
+  ^ ",diag="
+  ^ (String.concat "" (List.map (fun x -> cell_to_debug_string x) cell.diag))
+  ^ ",above="
+  ^ (String.concat "" (List.map (fun x -> cell_to_debug_string x) cell.above))
+  ^ "]"
 
-let get_above_of_cell cell  = match cell with
-  | ( _, _, _, above) -> above
+let rec last_list_element list =
+  match list with
+  | [] -> raise (Invalid_argument "Empty list")
+  | [last] -> last
+  | _::t -> last_list_element t
 
+(* build a cell of the indicated color, left cells, diagonal cells, and above cells *)
+let build_new_cell (color: color) (left_cell: cell) (diag_cell: cell) (above_cell: cell) = 
+  { color = color;
+    left = left_cell::left_cell.left;
+    diag = diag_cell::diag_cell.diag;
+    above = above_cell::above_cell.above }
 
 (* test is current cell can be of color color *)
-let rec can_be_of_color color left_cells diag_cells above_cells =
+let rec can_be_of_color (color: color) (left_cells: cell list) (diag_cells: cell list) (above_cells: cell list) =
 match left_cells with
 | [] -> true
 | hl::tl -> begin
@@ -24,6 +47,34 @@ match left_cells with
   | hd::td-> begin
     match above_cells with
     | [] -> true
-    | ha::ta -> (((get_color_of_cell hl) != color || (get_color_of_cell hd) != color || (get_color_of_cell ha) != color)  && ( can_be_of_color color tl td ta))
+    | ha::ta -> ((hl.color != color || hd.color != color || ha.color != color)  && ( can_be_of_color color tl td ta))
     end
   end
+
+let rec add_column_cell (consumer: cell list -> cell list -> unit) (last_column_remainder: cell list) (last_row: cell list) (new_column: cell list) =
+  (* print_newline();
+     print_endline "add_column_cell";
+     print_string  "last_column_remainder "; display_line last_column_remainder; print_newline() ;
+     print_string  "last_row ";              display_line last_row;              print_newline() ;
+     print_string  "new_column ";            display_line new_column;            print_newline() ; *)
+  match last_column_remainder with
+  | [] -> consumer new_column last_row 
+  | [last] -> ( consumer ( (build_new_cell Green (List.hd last_row) last (List.hd new_column)) :: new_column ) last_row; 
+                consumer ( (build_new_cell Blue  (List.hd last_row) last (List.hd new_column)) :: new_column ) last_row  )
+  | h::((i::_) as rest) -> ( add_column_cell consumer rest last_row ( (build_new_cell Green i h (List.hd new_column)) :: new_column ) ;
+                             add_column_cell consumer rest last_row ( (build_new_cell Blue  i h (List.hd new_column)) :: new_column ) )
+
+let add_column (consumer: cell list -> cell list -> unit) (last_column: cell list) (last_row: cell list)  =
+  (* print_newline();
+     print_endline "add_column";
+     print_string  "last_column ";        display_line last_column;         print_newline() ;
+     print_string  "last_row ";           display_line last_row;            print_newline() ; *)
+  match last_column with
+  | [] -> begin
+            let last_row_element = last_list_element last_row in
+              begin
+                add_column_cell consumer last_column last_row [ { color = Green; left = last_row_element::last_row_element.left; diag = []; above = [] } ] ;
+                add_column_cell consumer last_column last_row [ { color = Blue;  left = last_row_element::last_row_element.left; diag = []; above = [] } ]
+              end
+          end
+  | _ -> ()
--- ramsey2/test/ramsey.ml
+++ ramsey2/test/ramsey.ml
@@ -1,44 +1,55 @@
 open OUnit2
 open Ramsey
 
-let test1 = "cell accessors" >::: [
-  "get color of cell" >:: (fun _ -> assert_equal Green
-                                                 (get_color_of_cell (Green,
-                                                                     [ (Green, [], [], []); (Blue, [], [], []); (Blue, [], [], []) ],
-                                                                     [ (Green, [], [], []); (Blue, [], [], []); (Green, [], [], []) ],
-                                                                     [ (Blue, [], [], []); (Blue, [], [], []) ])));
-  "get left cells of cell" >:: (fun _ -> assert_equal [ (Green, [], [], []); (Blue, [], [], []); (Blue, [], [], []) ]
-                                                      (get_left_of_cell (Green,
-                                                                         [ (Green, [], [], []); (Blue, [], [], []); (Blue, [], [], []) ],
-                                                                         [ (Green, [], [], []); (Blue, [], [], []); (Green, [], [], []) ],
-                                                                         [ (Blue, [], [], []); (Blue, [], [], []) ])));
-  "get diagonal cells of cell" >:: (fun _ -> assert_equal [ (Green, [], [], []); (Blue, [], [], []); (Green, [], [], []) ]
-                                                          (get_diag_of_cell (Green,
-                                                                             [ (Green, [], [], []); (Blue, [], [], []); (Blue, [], [], []) ],
-                                                                             [ (Green, [], [], []); (Blue, [], [], []); (Green, [], [], []) ],
-                                                                             [ (Blue, [], [], []); (Blue, [], [], []) ])));
-  "get above cells of cell" >:: (fun _ -> assert_equal [ (Blue, [], [], []); (Blue, [], [], []) ]
-                                                       (get_above_of_cell (Green,
-                                                                           [ (Green, [], [], []); (Blue, [], [], []); (Blue, [], [], []) ],
-                                                                           [ (Green, [], [], []); (Blue, [], [], []); (Green, [], [], []) ],
-                                                                           [ (Blue, [], [], []); (Blue, [], [], []) ])));
-]
-
-let test2 = "color possibility" >::: [
+let test1 = "color possibility" >::: [
   "single cell" >:: (fun _ -> assert_equal true (can_be_of_color Green
                                                                  []
                                                                  []
                                                                  []));
   "possible" >:: (fun _ -> assert_equal true (can_be_of_color Green
-                                                               [ (Green, [], [], []); (Blue, [], [], []); (Blue, [], [], []) ]
-                                                               [ (Green, [], [], []); (Blue, [], [], []); (Green, [], [], []) ]
-                                                               [ (Blue, [], [], []); (Blue, [], [], []); (Blue, [], [], [])] ));
+                                                               [ {color = Green; left = []; diag = []; above = []}; { color = Blue; left = []; diag = []; above = []}; {color = Blue;  left = []; diag = []; above = []} ]
+                                                               [ {color = Green; left = []; diag = []; above = []}; { color = Blue; left = []; diag = []; above = []}; {color = Green; left = []; diag = []; above = []} ]
+                                                               [ {color = Blue;  left = []; diag = []; above = []}; { color = Blue; left = []; diag = []; above = []}; {color = Blue;  left = []; diag = []; above = []}] ));
   "impossible" >:: (fun _ -> assert_equal false (can_be_of_color Blue
-                                                                 [ (Green, [], [], []); (Blue, [], [], []); (Blue, [], [], []) ]
-                                                                 [ (Green, [], [], []); (Blue, [], [], []); (Green, [], [], []) ]
-                                                                 [ (Blue, [], [], []); (Blue, [], [], []); (Blue, [], [], [])] ));
+                                                                 [ {color = Green; left = []; diag = []; above = []}; { color = Blue; left = []; diag = []; above = []}; {color = Blue;  left = []; diag = []; above = []} ]
+                                                                 [ {color = Green; left = []; diag = []; above = []}; { color = Blue; left = []; diag = []; above = []}; {color = Green; left = []; diag = []; above = []} ]
+                                                                 [ {color = Blue;  left = []; diag = []; above = []}; { color = Blue; left = []; diag = []; above = []}; {color = Blue;  left = []; diag = []; above = []}] ));
   
 ]
+
+let consumer_accumulation: (cell list * cell list) list ref = ref ([]: (cell list * cell list) list)
+let consumer column row = (consumer_accumulation:= (!consumer_accumulation @ [(column, row)]))
+
+let cell_list_to_string (list: cell list) = String.concat "" (List.map (fun x -> cell_to_debug_string x) list)
+
+let column_row_pair_to_string ((col: cell list), (row: cell list)) = String.concat ""
+                                                                                 [ "column: ";
+                                                                                    (cell_list_to_string col);
+                                                                                    " row: ";
+                                                                                    (cell_list_to_string row)]
+                                        
+let column_row_pair_list_to_string (list: (cell list * cell list) list) = "\n" ^ (String.concat "\n" (List.map (fun x -> (column_row_pair_to_string x)) list))
+
+let test2 = "column addition" >::: [
+  "single green cell" >:: (fun _ -> begin
+                                      consumer_accumulation := ([]: (cell list * cell list) list);
+                                      add_column consumer [] [ { color = Green; left = []; diag =  []; above = [] } ];
+                                      assert_equal [ ([{ color = Green; left = [ { color = Green; left = []; diag = []; above = [] } ]; diag = []; above = [] }], [{ color = Green; left = []; diag =  []; above = [] }]);
+                                                     ([{ color = Blue;  left = [ { color = Green; left = []; diag = []; above = [] } ]; diag = []; above = [] }], [{ color = Green; left = []; diag =  []; above = [] }])]
+                                                   !consumer_accumulation
+                                                   ~printer:column_row_pair_list_to_string
+                                    end);
+  "single blue cell" >:: (fun _ -> begin
+                                      consumer_accumulation := ([]: (cell list * cell list) list);
+                                      add_column consumer [] [ { color = Blue; left = []; diag =  []; above = [] } ];
+                                      assert_equal [ ([{ color = Green; left = [ { color = Blue; left = []; diag = []; above = [] } ]; diag = []; above = [] }], [{ color = Blue; left = []; diag =  []; above = [] }]);
+                                                     ([{ color = Blue;  left = [ { color = Blue; left = []; diag = []; above = [] } ]; diag = []; above = [] }], [{ color = Blue; left = []; diag =  []; above = [] }])]
+                                                   !consumer_accumulation
+                                                   ~printer:column_row_pair_list_to_string
+                                   end);
+]
+
+
 
 
 let _ = run_test_tt_main test1
