2efec9fc8f8c09b79e2738a23fa96c5490ff15cc
Laurent Mazuré <laurent@gilgamesh>
2023-06-17 07:58:40 +0200

    Added simple sort

---
+++ simplest_sort.ml
@@ -0,0 +1,13 @@
+(* insert value "value" in sorted list "list" returning a sorted list *)
+let rec insert_value_in_sorted_list value list =
+  if (list = [])
+  then [ value ]
+  else if (value < List.hd list)
+  then value :: list
+  else (List.hd list) :: (insert_value_in_sorted_list value (List.tl list)) 
+
+(* sort a list *)
+let rec sort_list list =
+  if (list = [])
+  then []
+  else insert_value_in_sorted_list (List.hd list) (sort_list (List.tl list))
