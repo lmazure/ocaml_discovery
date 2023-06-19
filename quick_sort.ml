5f31a3e42fadb07e488eef91ff6e2c78f5e7c91b
Laurent Mazuré <laurent@gilgamesh>
2023-06-19 09:13:52 +0200

    Added quick sort

---
+++ quick_sort.ml
@@ -0,0 +1,29 @@
+type current_division = { pivot: int; smaller: int list; larger: int list; remainder: int list } 
+                
+(* recursively split a list into two sub-lists according to a pivot
+the first sublist contains the number smaller than the pivot
+the second sublist contain the number larger than the pivot *)        
+let rec divide current =
+  if (current.remainder = [])
+  then { pivot = current.pivot;
+         smaller = current.smaller;
+         larger = current.larger ;
+         remainder = []}
+  else if (( List.hd current.remainder ) > current.pivot)
+  then divide { pivot = current.pivot;
+                smaller = current.smaller;
+                larger = List.hd current.remainder :: current.larger ;
+                remainder = List.tl current.remainder}
+  else divide { pivot = current.pivot;
+                smaller = List.hd current.remainder :: current.smaller;
+                larger = current.larger;
+                remainder = List.tl current.remainder } 
+                                                        
+(* quick sort *)
+let rec quick_sort l =
+  if (l = [])
+  then []
+  else let current = divide {pivot = (List.hd l); smaller =  []; larger = []; remainder = (List.tl l)} in
+    (quick_sort current.smaller) @ [ List.hd l] @ (quick_sort current.larger)
+                                                  
+                                                  
